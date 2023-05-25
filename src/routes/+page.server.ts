377c31375d93529f50367e38c65ba25c0093e307
nminchow <noelminchow@gmail.com>
2023-05-25 11:17:57 -0500

    reimplement package

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types';
-import parse from 'rss-to-json';
+import parse from '../util/parse';
 
 export const load = (async () => {
   return {
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ command }: { command: string }) => {
-	const shared = {
-		plugins: [sveltekit()]
-	};
-  if (command === 'serve') {
-    return shared
-  } else {
-    return {
-			...shared,
-			ssr: {
-				noExternal: ['rss-to-json']
-			},
-    }
-  }
-})
+export default defineConfig({
+	plugins: [sveltekit()]
+});
