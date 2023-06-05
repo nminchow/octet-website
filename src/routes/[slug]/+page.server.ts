7a66e7c4d5c484299d93d97b9920ce518024543f
nminchow <noelminchow@gmail.com>
2023-06-05 10:46:34 -0500

    switch to acast feed

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -4,7 +4,9 @@ import parse from '../../util/parse';
 
 export const load = (async ({ params }) => {
 
-  const result = await parse('https://rss.octetpod.com');
+  // const result = await parse('https://rss.octetpod.com');
+  // something weird is happening in cloudflares env when we try to fetch this directly
+  const result = await parse('https://feeds.acast.com/public/shows/6452ab451ec047001147ca2f');
   const slug = parseInt(params.slug, 10);
 
   if (!result?.items || result?.items.length < slug) {
